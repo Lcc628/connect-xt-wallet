50e5d31fd1208a84b059de458a221d05a65a7eb5
chung <gn00745603@gmail.com>
2023-02-27 16:57:04 +0800

    nextjs setup

---
+++ connect-xt-wallet/features/home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Home() {
+  return (
+    <>
+      <h1>connect xt wallet</h1>
+      <button>connect xt wallet</button>
+    </>
+  )
+}
+
+export default Home;
